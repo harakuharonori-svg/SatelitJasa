714329c79adb6b3771d8e55178baf01b30591415
Arifrhmn24 <rahman20arief@gmail.com>
2025-08-13 14:35:18 +0700

    menyelesaikan desain autentikasi

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Registrasi</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+</head>
+
+<body
+    style="background-image: url('/image/backgroundsaya.jpg'); background-repeat: no-repeat;width:100%;height:100%;background-size: cover">
+    <div class="d-flex justify-content-center align-items-center" style="width: 100%;height:100dvh">
+        <form action="" style="box-shadow: 0 25px 50px -12px rgb(0 0 0 / 0.25);background-color: white " class="border p-5 rounded-4 ">
+            <div class="mb-4 text-center">
+                <img src="agagugug" alt="ugugagag">
+            </div>
+            <div class="mb-4">
+            <ul class="nav nav-pills nav-fill">
+                <li class="nav-item">
+                    <a class="nav-link " aria-current="page" href="/auth/login">Log in</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="/auth/register">Register</a>
+                </li>
+                </li>
+            </ul>
+        </div>
+            <div class="mb-4">
+                <h6>Masukkan data</h6>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Username</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="ContohNama">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            </div>
+
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="1234">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Repeat Password</label>
+                <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="1234">
+            </div>
+            <div class="mb-1">
+                <button class="btn rounded btn-primary" style="height:40px;width:100%;">Registrasi</button>
+            </div>
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/auth/login', [AuthController::class,"index"]);
+
+Route::get('/auth/register', action: [AuthController::class,"register"]);
